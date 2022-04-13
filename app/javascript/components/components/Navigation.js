b61d19dc33dad5bf85755a1ac7ef0ce97730808e
Marc Matias <mrcallenmatias@gmail.com>
2022-04-13 16:02:14 -0700

    home landing page

---
+++ app/javascript/components/components/Navigation.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navigation extends Component {
+    render() {
+        return(
+            <>
+                <h1>Navigation</h1>
+            </>
+        )
+    }
+}
